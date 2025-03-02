88bfbaab9207162f4e15b40beaa8b1bd4da273dc
ALERZO <alerzo@Lawrence-Edet.local>
2025-03-02 02:11:49 +0100

    I pused some corrrections after running npm run build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Hero from "@/components/Hero";
-import AboutUs from "@/components/About";
 import CourseCards from "@/components/Courses";
 
 export default function Home() {
