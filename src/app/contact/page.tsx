cfabf8701fbc9d3283ea7ccf71d591b78559fd2e
Lawrence Ime <lawrenceime246@gmail.com>
2025-03-19 07:08:20 +0100

    I added payments

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,26 +1,26 @@
 'use client'
 import { useForm, ValidationError } from '@formspree/react';
 import { MoveLeft } from 'lucide-react';
-import React,{useEffect , useState} from 'react';
-import {useRouter} from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import Select from 'react-select';
-import dynamic from   'next/dynamic'
+import dynamic from 'next/dynamic'
 import Image from 'next/image';
-// import HeroSection from './HeroSection';
+import { motion } from 'framer-motion';
 
-const  ContactForm = () => {
+
+const ContactForm = () => {
   const [state, handleSubmit] = useForm("xpwzeggj");
   const [countries, setCountries] = useState<Option[]>([]);
   const [selectedCode, setSelectedCode] = useState<string>('+1');
-  const [phone, setPhone] = useState<string>('');  // New state for phone
-
+  const [phone, setPhone] = useState<string>('');  // State for phone
 
   const router = useRouter();
-  
+
   const handleClick = () => {
     router.push('/');
   }
-  
+
   interface Country {
     flags: {
       svg: string;
@@ -52,7 +52,7 @@ const  ContactForm = () => {
               {`${country.name.common} (+${country.idd?.root || ''}${country.idd?.suffixes?.[0] || ''})`}
             </div>
           ),
-          
+
           value: `${country.idd?.root || ''}${country.idd?.suffixes?.[0] || ''}`,
         })).filter(option => option.value); // Filter out countries without codes
         setCountries(options);
@@ -60,7 +60,7 @@ const  ContactForm = () => {
         console.error('Error fetching country codes:', error);
       }
     };
-  
+
     fetchCountries();
   }, []);
 
@@ -68,39 +68,59 @@ const  ContactForm = () => {
     e.preventDefault();
     const form = e.currentTarget as HTMLFormElement;
     const formData = new FormData(form);
-  
+
     // Combine country code + phone number
     const fullPhoneNumber = `${selectedCode}${phone}`;
     formData.set('phone', fullPhoneNumber);  // Replace phone with full number
-  
+
     // Submit updated form data to Formspree
     handleSubmit(formData as unknown as HTMLFormElement);
   };
-  
+
 
   if (state.succeeded) {
-    console.log(state);
-    
     return (
-     <>
-     <button className="  absolute top-3/2 left-2 p-4 bg-white rounded-[50px] w-[50px] h-[50px] text-center mt-5" onClick={handleClick}>
-        <MoveLeft color='black' width={24} height={24}/>
-     </button>
-      <div className="flex justify-center items-center h-screen">
-        <p className="text-xl text-green-600">Thanks for your submission! We will be in touch!.</p>
-      </div>
-     </>
+      <>
+        <button className="absolute top-3/2 left-2 p-4 bg-white rounded-[50px] w-[50px] h-[50px] text-center mt-5" onClick={handleClick}>
+          <MoveLeft color='black' width={24} height={24} />
+        </button>
+        <div className="flex flex-col gap-4 justify-center items-center h-screen">
+          <p className="text-xl text-green-600">Thanks for your submission!</p>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className="mt-4 p-6 bg-blue-100 rounded-lg shadow-md"
+          >
+            <h3 className="text-lg font-semibold text-blue-700">Payment Details</h3>
+            <p className="text-sm text-gray-700">Account Name: Mathias Osekwemen Iyoha</p>
+            <p className="text-sm text-gray-700">Account Number: 1715807027</p>
+            <p className="text-sm text-gray-700">Bank: Access Bank</p>
+            <div className='mt-4'>
+            <a
+              href="https://wa.me/2348133811788" 
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-4 py-2 mt-5 bg-green-500 text-white rounded-lg"
+            >
+              Notify Us
+            </a>
+            </div>
+          </motion.div>
+        <button className='mt-8 font-semibold text-black' onClick={handleClick}>Back To Home page</button>
+        </div>
+      </>
     );
   }
 
   return (
-    <div className="bg-cover bg-center bg-fixed min-h-screen" style={{ backgroundImage: 'url(/assets/emmanuel-ikwuegbu-6BWFAPPWn-8-unsplash.jpg)',  }}>
+    <div className="bg-cover bg-center bg-fixed min-h-screen" style={{ backgroundImage: 'url(/assets/emmanuel-ikwuegbu-6BWFAPPWn-8-unsplash.jpg)', }}>
       <form onSubmit={handleSubmitWithCode} className="max-w-2xl mx-auto pt-36 p-6 space-y-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {/* First Name Field */}
           <div className="flex flex-col">
-            <label htmlFor="firstName" className="mb-2">
-              First Name <span className="text-red-500">*</span> 
+            <label htmlFor="firstName" className="mb-2 text-white font-semibold">
+              First Name <span className="text-red-500">*</span>
             </label>
             <input
               id="firstName"
@@ -114,7 +134,7 @@ const  ContactForm = () => {
 
           {/* Last Name Field */}
           <div className="flex flex-col">
-            <label htmlFor="lastName" className="mb-2">
+            <label htmlFor="lastName" className="mb-2 text-white font-semibold">
               Last Name <span className="text-red-500">*</span>
             </label>
             <input
@@ -129,7 +149,7 @@ const  ContactForm = () => {
 
           {/* Email Field */}
           <div className="flex flex-col">
-            <label htmlFor="email" className="mb-2">
+            <label htmlFor="email" className="mb-2 text-white font-semibold">
               Email <span className="text-red-500">*</span>
             </label>
             <input
@@ -144,7 +164,7 @@ const  ContactForm = () => {
 
           {/* Organization Field */}
           <div className="flex flex-col">
-            <label htmlFor="organization" className="mb-2">
+            <label htmlFor="organization" className="mb-2 text-white font-semibold">
               Occupation
             </label>
             <input
@@ -158,7 +178,7 @@ const  ContactForm = () => {
 
           {/* Address Field */}
           <div className="flex flex-col">
-            <label htmlFor="address" className="mb-2">
+            <label htmlFor="address" className="mb-2 text-white font-semibold">
               Address <span className="text-red-500">*</span>
             </label>
             <input
@@ -173,7 +193,7 @@ const  ContactForm = () => {
 
           {/* Phone Number Field */}
           <div className="flex flex-col">
-            <label htmlFor="phone" className="mb-2">
+            <label htmlFor="phone" className="mb-2 text-white font-semibold">
               Phone Number <span className="text-red-500">*</span>
             </label>
             <div className="flex">
@@ -199,7 +219,7 @@ const  ContactForm = () => {
 
           {/* Age Range Field */}
           <div className="flex flex-col">
-            <label htmlFor="ageRange" className="mb-2">
+            <label htmlFor="ageRange" className="mb-2 text-white font-semibold">
               Age Range <span className="text-red-500">*</span>
             </label>
             <select
@@ -219,7 +239,7 @@ const  ContactForm = () => {
 
           {/* Previous Experience Field */}
           <div className="flex flex-col">
-            <label htmlFor="experience" className="mb-2">
+            <label htmlFor="experience" className="mb-2 text-white font-semibold">
               Any Previous Experience? <span className="text-red-500">*</span>
             </label>
             <select
@@ -237,23 +257,23 @@ const  ContactForm = () => {
 
           {/* Specify Experience Field */}
         </div>
-          <div className="flex flex-col">
-            <label htmlFor="experienceDetails" className="mb-2">
-              If yes, please specify
-            </label>
-            <textarea
-              id="experienceDetails"
-              name="experienceDetails"
-              rows={4}
-              className="border p-2 text-black rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            
-            <ValidationError prefix="Experience Details" field="experienceDetails" errors={state.errors} />
-          </div>
+        <div className="flex flex-col">
+          <label htmlFor="experienceDetails" className="mb-2 text-white font-semibold">
+            If yes, please specify
+          </label>
+          <textarea
+            id="experienceDetails"
+            name="experienceDetails"
+            rows={4}
+            className="border p-2 text-black rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+
+          <ValidationError prefix="Experience Details" field="experienceDetails" errors={state.errors} />
+        </div>
 
         {/* Region Field */}
         <div className="flex flex-col">
-          <label htmlFor="region" className="mb-2">
+          <label htmlFor="region" className="mb-2 text-white font-semibold">
             Region <span className="text-red-500">*</span>
           </label>
           <select
@@ -274,8 +294,8 @@ const  ContactForm = () => {
 
         {/* How can we help field */}
         <div className="flex flex-col">
-          <label htmlFor="message" className="mb-2">
-            How can we help you? 
+          <label htmlFor="message" className="mb-2 text-white font-semibold">
+            How can we help you?
           </label>
           <textarea
             id="message"
@@ -293,7 +313,7 @@ const  ContactForm = () => {
             disabled={state.submitting}
             className="bg-blue-600 text-white px-8 py-2 rounded hover:bg-blue-700 disabled:bg-blue-300"
           >
-            Submit
+            {state.submitting ? "Loading..." : "Submit"}
           </button>
         </div>
       </form>
